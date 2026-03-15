1fc65d9acafe97da5ec00b59d1851289798cea81
Abner A Ananias <abnerndr.dev@gmail.com>
2026-03-15 11:58:21 -0300

    Add Nixpacks configuration, update profile data, and enhance homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,54 +8,77 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-	const [isClient, setIsClient] = useState(false);
+  const [isClient, setIsClient] = useState(false);
 
-	useEffect(() => {
-		setIsClient(true);
-	}, []);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-	if (!isClient) {
-		return null; // or a loading spinner
-	}
-	return (
-		<div className="font-[family-name:var(--font-geist-sans)] w-full min-h-screen bg-background">
-			<main className="mx-auto mt-20 w-full flex flex-col justify-center items-center">
-				<Image src={ProfileData.picture} alt="Abner" width={224} height={224} className="mx-auto size-56 rounded-full object-cover" />
-				<div className="flex flex-row items-center gap-1 mt-6">
-					<h3 className=" text-base/7 font-semibold tracking-tight text-foreground">{ProfileData.name}</h3>
-				</div>
-				<p className="text-sm/6 text-foreground/70">{ProfileData.title}</p>
-				<ul className="w-full h-full flex flex-col px-10 mt-10 gap-y-4">
-					{items.map((item, index) => (
-						<li key={index + 1}>
-							<Link className="flex items-center justify-between gap-6 p-2 bg-foreground/5 hover:bg-foreground/10 rounded-lg" href={item.href}>
-								<div className="flex flex-row justify-between items-center w-full">
-									<div className="flex flex-row items-center gap-4">
-										<item.icon className="size-10 text-foreground/90" />
-										<div>
-											<h4 className="text-base/7 font-semibold text-foreground">{item.name}</h4>
-											<p className="text-sm/6 text-foreground/70">{item.title}</p>
-										</div>
-									</div>
-									<ChevronRightIcon size={24} className="text-primary/40" />
-								</div>
-							</Link>
-						</li>
-					))}
-				</ul>
-			</main>
-			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-10">
-				<ul role="list" className="mt-6 flex justify-center gap-x-6">
-					{socials.map((social, index) => (
-						<li key={index + 1}>
-							<Link href={social.redirectUrl} target="_blank" className="text-primary/40 hover:text-primary/65">
-								<span className="sr-only">{social.name}</span>
-								{social.icon}
-							</Link>
-						</li>
-					))}
-				</ul>
-			</footer>
-		</div>
-	);
+  if (!isClient) {
+    return null; // or a loading spinner
+  }
+
+  const profilePicture = ProfileData.picture;
+
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)] w-full min-h-screen bg-background">
+      <main className="mx-auto mt-20 w-full flex flex-col justify-center items-center">
+        <Image
+          src={profilePicture}
+          alt="Abner"
+          width={profilePicture.width}
+          height={profilePicture.height}
+          className="mx-auto size-56 rounded-full object-cover"
+        />
+        <div className="flex flex-row items-center gap-1 mt-6">
+          <h3 className=" text-base/7 font-semibold tracking-tight text-foreground">
+            {ProfileData.name}
+          </h3>
+        </div>
+        <p className="text-sm/6 text-foreground/70">{ProfileData.title}</p>
+        <ul className="w-full h-full flex flex-col px-10 mt-10 gap-y-4">
+          {items.map((item, index) => (
+            <li key={index + 1}>
+              <Link
+                className="flex items-center justify-between gap-6 p-2 bg-foreground/5 hover:bg-foreground/10 rounded-lg"
+                href={item.href}
+                target="_blank"
+              >
+                <div className="flex flex-row justify-between items-center w-full">
+                  <div className="flex flex-row items-center gap-4">
+                    <item.icon className="size-10 text-foreground/90" />
+                    <div>
+                      <h4 className="text-base/7 font-semibold text-foreground">
+                        {item.name}
+                      </h4>
+                      <p className="text-sm/6 text-foreground/70">
+                        {item.title}
+                      </p>
+                    </div>
+                  </div>
+                  <ChevronRightIcon size={24} className="text-primary/40" />
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center mt-10">
+        <ul role="list" className="mt-6 flex justify-center gap-x-6">
+          {socials.map((social, index) => (
+            <li key={index + 1}>
+              <Link
+                href={social.redirectUrl}
+                target="_blank"
+                className="text-primary/40 hover:text-primary/65"
+              >
+                <span className="sr-only">{social.name}</span>
+                {social.icon}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    </div>
+  );
 }
--- src/lib/helpers/profile.ts
+++ src/lib/helpers/profile.ts
@@ -1,8 +1,8 @@
-import Me from "@/image/me.jpg";
+import Me from "@/image/3388.jpg";
 import { PROFILE_DESCRIPTION, PROFILE_NAME } from "../constants/env";
 
 export const ProfileData = {
-	name: PROFILE_NAME,
-	title: PROFILE_DESCRIPTION,
-	picture: Me,
+  name: PROFILE_NAME,
+  title: PROFILE_DESCRIPTION,
+  picture: Me,
 };
